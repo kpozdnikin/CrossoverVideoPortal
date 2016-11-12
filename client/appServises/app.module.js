32a1f89da349988170dbeb2d2e6fd3685a72b1fc
Konstantin Pozdnikin <kpozdnikin@gmail.com>
2016-11-12 14:08:51 +0300

    Create first page with routers #1 30m

---
+++ client/appServises/app.module.js
@@ -0,0 +1,13 @@
+(function () {
+    'use strict';
+
+    angular.module('videoPortal', [
+        'ui.router',
+        'ngAnimate',
+        'ngCookies',
+        'ngMaterial',
+        'videoPortal.home',
+        'videoPortal.item'
+    ]);
+
+})();
